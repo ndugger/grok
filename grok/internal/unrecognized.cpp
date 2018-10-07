f72452112b7897cb272e6890acecb4544bfd9d29
Nick Dugger <email@nickdugger.com>
2018-10-07 12:19:18 -0500

    flushing out idea to generate cmake

--- grok/internal/unrecognized.cpp
+++ grok/internal/unrecognized.cpp
@@ -7,11 +7,9 @@ using namespace std;
 
 namespace grok::internal {
 
-    string application_name = "grok";
-
     int unrecognized (string command_name) {
-        cout << application_name << " unrecognized command: " << command_name << endl;
-        cout << "try '" << application_name << " help' for a list of available commands" << endl;
+        cout << "unrecognized command: " << command_name << endl;
+        cout << "try 'grok help' for a list of available commands" << endl;
 
         return 1;
     }
