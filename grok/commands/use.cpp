46deb798ba94f0beaeeffab1670641227898511e
Nick Dugger <email@nickdugger.com>
2018-10-06 23:43:00 -0500

    now checks if file exists, then clones repo from json

--- grok/commands/use.cpp
+++ grok/commands/use.cpp
@@ -3,16 +3,30 @@
 # include <iostream>
 # include <string>
 
-# include "git2/clone.h"
+# include "git2/repository.h"
+# include "nlohmann/json.hpp"
+
+# include "grok/internal/utilities.cpp"
 
 using namespace std;
+using namespace nlohmann;
+using namespace grok::internal;
 
 namespace grok::commands {
 
     int use (string arguments[]) {
-        string repository = arguments[ 0 ];
+        string package_name = arguments[ 0 ];
 
-        cout << repository << endl;
+        if (registry_contains(package_name)) {
+            json package_info = package_from_registry(package_name);
+            string package_release = package_info[ "package" ][ "release" ];
+            string package_repository = package_info[ "package" ][ "repository" ];
+
+            git_repository* repository = clone_package_repository(package_name, package_repository, package_release);
+        }
+        else {
+
+        }
 
         return 0;
     }
