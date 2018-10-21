3469b6ff52083510966be64fd10b9a310d654929
Nick Dugger <email@nickdugger.com>
2018-10-21 03:32:52 -0500

    incorporated libgit build into install, deleted separate build sh

--- scripts/build_libgit2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd libraries/git2/
-
-mkdir build/
-cd build/
-
-cmake ..
-cmake --build .
--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -1,22 +1,37 @@
 #!/usr/bin/env bash
 
+#### install grok in top level /opt/ directory
 cd /opt/
 
+#### download grok source files
 git clone --recurse-submodules https://github.com/fyrware/grok.git
 cd grok/
 
-sh scripts/build_libgit2.sh
+#### build libgit2
+cd libraries/git2/
 
+mkdir build/
+cd build/
+
+cmake ..
+cmake --build .
+
+cd ../../../
+
+#### create CMakeLists.txt file
 touch CMakeLists.txt
 echo "cmake_minimum_required(VERSION 3.10)" >> CMakeLists.txt
 echo "include(.grokmake)" >> CMakeLists.txt
 
+#### create output binary folder
 mkdir binary/
 cd binary/
 
+#### build grok
 cmake ..
 cmake --build . --target all --clean-first
 
+#### add grok to PATH
 echo -e "\n" >> ~/.bashrc
 echo "PATH=\"\$PATH:/opt/grok/binary\"" >> ~/.bashrc
 source ~/.bashrc
